2005548fd06f247ebe9144935ae1ee4b00c99956
Sofia-Madryha <75246511+Sofia-Madryha@users.noreply.github.com>
2023-08-25 14:10:45 +0000

    Deploying to gh-pages from @ Sofia-Madryha/goit-js-hw-09@e51d5183004668964a156c947919c5b23cf7238c 🚀

---
+++ 01-color-switcher.82dfbc69.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),o=document.querySelector("body");t.addEventListener("click",(function(){var n=setInterval((function(){t.disabled=!0,console.log(o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)))}),1e3);e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}))}))}();
+//# sourceMappingURL=01-color-switcher.82dfbc69.js.map
