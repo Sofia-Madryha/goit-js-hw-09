d3ba5c78f3f09ed74446be957df22f0229827474
sofimadryha <75246511+Sofia-Madryha@users.noreply.github.com>
2023-08-26 21:01:02 +0300

    add the 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+const form = document.querySelector('form');
+const btnSubmit = document.querySelector('button');
+
+btnSubmit.addEventListener('sumbit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const step = Number(form.elements.step.value);
+  const amount = Number(form.elements.amount.value);
+
+
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i, delay);
+    delay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+promise
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
 }
